9a9a4cad562853faee01f3ee78bf8bc308ac72e1
Mayur Dhotre <73772313+mayur4279@users.noreply.github.com>
2024-06-05 18:34:38 +0530

    Update 02-Tomcat_setup.sh

--- 02-Tomcat_setup.sh
+++ 02-Tomcat_setup.sh
@@ -5,18 +5,18 @@ sudo yum install java -y
 sudo git clone https://github.com/mayur4279/tmp /opt/tmp
 
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.zip -P /opt
-sudo unzip /opt/apache-tomcat-9.0.87.zip -d /opt
+sudo unzip /opt/apache-tomcat-9.0.89.zip -d /opt
 
-sudo cp -rvf /opt/tmp/student.war /opt/apache-tomcat-9.0.87/webapps
+sudo cp -rvf /opt/tmp/student.war /opt/apache-tomcat-9.0.89/webapps
 
-sudo cp -rvf /opt/tmp/mysql-connector.jar /opt/apache-tomcat-9.0.87/lib
+sudo cp -rvf /opt/tmp/mysql-connector.jar /opt/apache-tomcat-9.0.89/lib
 
-sudo chmod +x /opt/apache-tomcat-9.0.87/bin/catalina.sh
+sudo chmod +x /opt/apache-tomcat-9.0.89/bin/catalina.sh
  
 sudo  rm -rvf  /opt/tmp   
-sudo  rm -rvf  /opt/apache-tomcat-9.0.87.zip 
+sudo  rm -rvf  /opt/apache-tomcat-9.0.89.zip 
  
 # Start Tomcat
-sudo bash /opt/apache-tomcat-9.0.87/bin/catalina.sh start
+sudo bash /opt/apache-tomcat-9.0.89/bin/catalina.sh start
 
 

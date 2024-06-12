c75c3326af25ebc4fbc3b211b3558aa157cb9db1
Mayur Dhotre <73772313+mayur4279@users.noreply.github.com>
2024-06-12 19:06:46 +0530

    added sudo  in 2 line

--- 04-jenkins-installation.sh
+++ 04-jenkins-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update -y
-wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
 sudo yum install java-17-amazon-corretto -y
